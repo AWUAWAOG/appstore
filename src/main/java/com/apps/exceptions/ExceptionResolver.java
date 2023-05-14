0f6b309474b4195db8fcf545a665fa86a5094c7a
AWUAWA <rimuryt200201@gmail.com>
2023-05-14 22:37:08 +0300

    ExceptionResolver

--- src/main/java/com/apps/exceptions/ExceptionResolver.java
+++ src/main/java/com/apps/exceptions/ExceptionResolver.java
@@ -3,18 +3,26 @@ package com.apps.exceptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/*@ControllerAdvice
+@ControllerAdvice
 public class ExceptionResolver {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ArithmeticException.class) {
-        public String myHandler (Exception e){
-            logger.warn("ArithmeticException" + e);
-            return "unsuccessfully";
-        }
-    }*/
+    @ExceptionHandler(ArithmeticException.class)
+    public String myHandler(Exception e) {
+        logger.warn("ArithmeticException" + e);
+        return "unsuccessfully";
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<HttpStatus> usernameNotFound(Exception e) {
+        logger.warn(e.getMessage());
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+}
