8a950935a16828dc26e74e2c4113477e9717acd8
AWUAWA <rimuryt200201@gmail.com>
2023-05-14 22:36:00 +0300

    Spring SecurityService.java

---
+++ src/main/java/com/apps/security/SecurityService.java
@@ -0,0 +1,37 @@
+package com.apps.security;
+
+import com.apps.domain.User;
+import com.apps.domain.request.AuthRequest;
+import com.apps.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SecurityService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityService(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String getTokenFromRequest(AuthRequest authRequest) {
+        Optional<User> user = userRepository.findUserByUserLogin(authRequest.getLogin());
+        if (user.isPresent() && passwordEncoder.matches(authRequest.getPassword(), user.get().getUserPassword())) {
+            logger.warn(user.toString());
+            return jwtService.createJwtToken(authRequest.getLogin());
+        }
+        logger.warn(user.toString());
+        return "";
+    }
+}
