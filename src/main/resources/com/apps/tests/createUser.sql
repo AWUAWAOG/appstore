6467576c0b7c08d114de5b87e5259100b45d65f2
AWUAWA <rimuryt200201@gmail.com>
2023-05-28 22:01:22 +0300

    createUser.sql

---
+++ src/main/resources/com/apps/tests/createUser.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+    users (id, user_login, user_password, email, first_name, last_name, created, edited, is_deleted, role)
+VALUES
+    (DEFAULT, 'LOGIN123', 'pass', 'email@gmail.com', 'FN', 'LN', 2020-02-02, 2020-04-04, FALSE, 'USER');
