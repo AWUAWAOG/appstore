5849ba6941168ca4354e8a74e57f96204a8204bb
AWUAWA <rimuryt200201@gmail.com>
2023-05-30 18:13:57 +0300

    RepositoriesTest update

--- src/test/java/RepositoriesTest.java
+++ src/test/java/RepositoriesTest.java
@@ -1,13 +1,9 @@
-package com.apps.tests;
-
 import com.apps.domain.Application;
 import com.apps.domain.Developer;
 import com.apps.domain.User;
-import com.apps.repository.ApplicationRepository;
 import com.apps.repository.UserRepository;
 import lombok.NoArgsConstructor;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -19,25 +15,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 @NoArgsConstructor
 public class RepositoriesTest {
 
+    public UserRepository userRepository;
+
+    public Application application = new Application();
     public Developer developer = new Developer();
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ApplicationRepository applicationRepository;
 
     @Test
     @Sql("createUser.sql")
     public void createUserTest() {
-        userRepository.save(new User());
+        User user = new User();
+        userRepository.save(user);
         assertThat(userRepository).isNotNull();
     }
 
     @Test
     public void ApplicationRepositoryIsNotNull() {
-        applicationRepository.save(new Application());
-        assertThat(new Application()).isNotNull();
+        if (application == null) {
+            throw new IllegalArgumentException();
+        } else
+            System.out.println("ApplicationRepositoryIsNotNull test done!");
     }
 
     @Test
